0da97a1ea5d34f2a27547f392a76f883c3c22437
EgorMochalov <egor18mochalov@yandex.ru>
2025-10-08 13:41:27 +0300

    first commit

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,7 @@
 // models/index.js
 const { Sequelize } = require('sequelize');
-const config = require('../config/database.js')[process.env.NODE_ENV || 'development'];
-
+const config = require('../config/database.js')[process.env.NODE_ENV || 'production'];
+console.log(config)
 const sequelize = new Sequelize(config);
 
 const db = {
